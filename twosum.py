7275a4e0bc31a115ab797bcdaf35f6bfcc541681
Prashanth Pothugunta <pothuguntaprashanth84@gmail.com>
2025-08-12 09:19:46 +0530

    adding my first dsa problem on 180 days of dsa challenge

---
+++ twosum.py
@@ -0,0 +1,7 @@
+n=int(input())
+arr=list(map(int,input().split()))
+target=int(input())
+for i in range(len(arr)):
+    for j in range(i+1,len(arr)):
+        if arr[i]+arr[j]==target:
+            print("Pair found:",i,j)  
