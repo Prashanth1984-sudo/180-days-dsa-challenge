3d8aa612fcbda5538c2f25b5139236f758a209bc
Prashanth Pothugunta <pothuguntaprashanth84@gmail.com>
2025-08-13 18:43:18 +0530

    Update Find the Middle Index in Array.py

--- day2/Find the Middle Index in Array.py
+++ day2/Find the Middle Index in Array.py
@@ -9,3 +9,17 @@ for i in range(len(arr)):
     left_sum+=arr[i]
 else:
     print(-1)
+# Leetcode solution
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        sum_not_div = 0
+        sum_div = 0
+        
+        for i in range(1, n + 1):
+            if i % m != 0:
+                sum_not_div += i
+            else:
+                sum_div += i
+        
+        return sum_not_div - sum_div
+
